7fed251f702498d2759ba44dd954d4e1130eceba
mimi2709 <73721914+mimi2709@users.noreply.github.com>
2021-06-08 17:45:29 -0400

    added NetflixClone

---
+++ Netflix_Clone/build/precache-manifest.9d51b626f08abddf9974eacbac380326.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d44621bfd1b64dabf146d615d69d178",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58cd5f91a7f02c03b21e",
+    "url": "/static/css/main.e32ee1f8.chunk.css"
+  },
+  {
+    "revision": "92f2502db03248df14ab",
+    "url": "/static/js/2.7199c33c.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.7199c33c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58cd5f91a7f02c03b21e",
+    "url": "/static/js/main.cb3ddd0d.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
---
+++ Netflix_Clone/src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "4be28ef4207eb4d51e9bcd6f5cd30bf2";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchAnimationMovies: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  // fetchDocumentaryMovies: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+export default requests;
